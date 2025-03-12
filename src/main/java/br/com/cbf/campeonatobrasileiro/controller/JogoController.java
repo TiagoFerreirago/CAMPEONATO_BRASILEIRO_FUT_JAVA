90370a6b7c9733a36c04ad07eb517b97ff6a3238
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-03-11 22:01:13 -0300

    adding jogocontroller

---
+++ src/main/java/br/com/cbf/campeonatobrasileiro/controller/JogoController.java
@@ -0,0 +1,25 @@
+package br.com.cbf.campeonatobrasileiro.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cbf.campeonatobrasileiro.service.JogoService;
+
+@RestController
+@RequestMapping(value = "/jogos")
+public class JogoController {
+
+	@Autowired
+	private JogoService jogoService;
+	
+	@PostMapping(value = "/gerar-jogos")
+	public ResponseEntity<?>gerarJogos(){
+		jogoService.jogos(LocalDateTime.now());
+		return ResponseEntity.ok().body(LocalDateTime.now());
+	}
+}
